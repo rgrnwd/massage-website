15c9ab1132d183a468019aed39e00eefc64d81ae
Roni Greenwood <ronig@squareup.com>
2023-09-03 08:32:27 +0300

    Add About and Services content

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,7 +3,29 @@ import React from 'react'
 const About: React.FC = () => {
     return (
         <div style={{ position: 'relative', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-            <h1 style={{ fontSize: '4em' }}>About Us</h1>
+            <h1 style={{ fontSize: '4em' }}>About Me</h1>
+            <div className="text-content">
+                I am a Remedial Massage therapist with a passion for helping people live and function at their best. 
+                I earned my Diploma of Remedial Massage from Victoria University in 2020. 
+                During my studies, I undertook a paid cadetship with the Western Bulldogs AFL team. 
+                I am also qualified in dry needling and traditional Thai massage. 
+                Currently I am studying for my Bachelor of Health Sciences in clinical myotherapy.
+            </div>
+            <div className="text-content">
+                I came to realise the benefits of regular massage treatment, while living in Bangkok and training martial arts full time. 
+                During 2017-18 I undertook several Thai Massage courses at the Wat Po Traditional Thai Medicine school and 
+                began practicing my new-found skills on my coach and teammates at Bangkok Fight Lab. 
+                I ran my own home-based Thai massage practice after moving to Munich, Germany.
+            </div>
+            <div className="text-content">
+                On returning home to Melbourne, I wasted no time in becoming a qualified Remedial Massage therapist. 
+                A keen problem solver, I believe in finding and addressing the root cause of a condition, 
+                rather than just treating the symptoms. My treatments can include a mix of Remedial and traditional Thai techniques, 
+                myofascial cupping, dry needling or mobility work, as well as posture and movement assessment & corrections 
+                which are derived largely from more than a decade of Tai Chi practice. 
+                I have worked with a variety of clients from professional athletes to office workers. 
+                I work with several elite level football teams including Essendon Bombers, Melbourne Storm and Melbourne Rebels. 
+            </div>
         </div>
     )
 }
--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -4,6 +4,41 @@ const Services: React.FC = () => {
     return (
         <div style={{ position: 'relative', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
             <h1 style={{ fontSize: '4em' }}>Services</h1>
+            <span className="services-header">Traditional Thai Massage</span>
+            <div className="text-content">
+                I learned it the traditional way; performed on a mat at floor level, fully dressed, 
+                no lotions or creams. This is what I did for three years, before studying remedial massage. 
+                I still enjoy practicing this way, and I do incorporate many Thai techniques (or variations) 
+                into my remedial massage work. 
+                The techniques and theory of Thai massage are comparable with modern anatomical understanding.
+            </div>
+            <span className="services-header">Remedial Massage</span>
+            <div className="text-content">
+                No text yet
+            </div>
+            <span className="services-header">Myofascial Cupping</span>
+            <div className="text-content">
+                This involves plastic or glass cups being placed on the body and attached by means of suction-induced vacuum. 
+                That&apos;s those red/purple circles you sometimes see on people. 
+                There are a few methods of using these cups, including leaving them in place, gliding them over an area of the body 
+                or placing them on the client while getting him or her to move the affected area. All these methods work by opening space between the skin, fascia and muscles, allowing fresh blood to into the area.
+
+            </div>
+            <span className="services-header">Dry Needling</span>
+            <div className="text-content">
+                A method of precisely targeting a trigger point or other sore area. 
+                This involves inserting acupuncture-type needles into the muscles, 
+                to really get to those tight and sore places. Unlike acupuncture however, 
+                dry needling is used only for soft tissue (i.e. muscle and connective tissue) conditions.
+            </div>
+            <span className="services-header">Posture and Movement</span>
+            <div className="text-content">
+                No text yet
+            </div>
+            <span className="services-header">Pregnancy</span>
+            <div className="text-content">
+                I offer pregnancy massage from twelve weeks to full-term. 
+            </div>
         </div>
     )
 }
