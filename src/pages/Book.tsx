a8f7bde40e8e95eff7f2d709e307a6e71b4b5c59
Roni Greenwood <ronig@squareup.com>
2023-09-10 11:43:31 +0300

    Update book and contact pages

--- src/pages/Book.tsx
+++ src/pages/Book.tsx
@@ -3,6 +3,10 @@ import React from 'react'
 const Book: React.FC = () => {
     return <div className="page">
         <h1 className="page-header">Book Online</h1>
+        <div className="text-content">
+            For bookings outside of the availability hours listed below, 
+            please contact me via email / SMS as I may be able to accommodate.
+        </div>
     </div>
 }
 
