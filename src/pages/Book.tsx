b593ff139cd705e4a15f4c7e7b6e431c43b580f7
Roni Greenwood <ronig@squareup.com>
2023-09-10 11:56:01 +0300

    Book redirect to Contact

--- src/pages/Book.tsx
+++ src/pages/Book.tsx
@@ -5,7 +5,7 @@ const Book: React.FC = () => {
         <h1 className="page-header">Book Online</h1>
         <div className="text-content">
             For bookings outside of the availability hours listed below, 
-            please contact me via email / SMS as I may be able to accommodate.
+            please <a href="/contact">contact me</a> as I may be able to accommodate.
         </div>
     </div>
 }
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,6 +4,13 @@ import ContactForm from '../components/ContactForm'
 const Contact: React.FC = () => {
     return <div className="page">
         <h1 className="page-header">Contact</h1>
+        <div className="text-content">
+            For bookings outside of the availability hours and other inquiries, 
+            please contact me using the form below.
+        </div>
+        <div className="text-content">
+            You can also contact me directly via email <b>duncan@dgmassage.com.au</b> or phone <b>0433 974 738</b>
+        </div>
         <ContactForm/>
     </div>
 }
