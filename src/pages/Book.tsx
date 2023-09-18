503fad0a5d56ce8207d1c3061e0c0209d2132306
rgrnwd <rgrnwd@thoughtworks.com>
2023-09-18 19:17:05 +1000

    Update wording

--- src/pages/Book.tsx
+++ src/pages/Book.tsx
@@ -7,6 +7,9 @@ const Book: React.FC = () => {
             For bookings outside of the availability hours listed below, 
             please <a href="/contact">contact me</a> as I may be able to accommodate.
         </div>
+        <div className="text-content">
+            If you would prefer me to treat you at your home, please send me an enquiry.
+        </div>
         <iframe className="bookings" src="https://www.halaxy.com/book/widget/duncan-greenwood/location/1000241" />
     </div>
 }
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -5,8 +5,7 @@ const Contact: React.FC = () => {
     return <div className="page">
         <h1 className="page-header">Contact</h1>
         <div className="text-content">
-            For bookings outside of the availability hours and other inquiries, 
-            please contact me using the form below.
+            For all enquiries, please contact me using the form below.
         </div>
         <div className="text-content">
             You can also contact me directly via email <a href="mailto:duncan@dgmassage.com.au"><b>duncan@dgmassage.com.au</b></a> or phone <b>0433 974 738</b>
